f1346296cf3c719f0b1fb09da495ef05df34b05c
dominik <dominik6746@gmail.com>
2022-09-26 23:53:51 +0200

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 const MyDocument = () => {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
-        <meta name='description' content='Currency exchange app' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Currency exchange rates app" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
         <Main />
